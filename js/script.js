614d948c3b0488817395e07a5aa193577a6bd736
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-03-26 14:31:14 +0100

    add media query for mobile

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+/*
+function screen(size) {
+    if(size.matches) {
+        var element = document.getElementsById("menu-toggle");
+        element.classList.toggle("menu-disabled");
+    }
+}
+
+var size = window.matchMedia("(max-width: 425px)");
+
+screen(size);
+size.addListener(screen);
+*/
