f970ceeb91443e31b95cd0a750c9ab94defbe3cf
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-22 13:23:03 +0200

    Add hide/show menu fullscreen

--- js/script.js
+++ js/script.js
@@ -5,11 +5,25 @@ function toggleMenu(visible) {
    for (var i = 0; i < elements.length; i++) {
        elements[i].classList.toggle('show', visible);
    }
-   
-
   }
+
+  function resize(visible) {
+    var elements = document.querySelectorAll('.your-manager-photo');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle('your-manager-photo-2', visible);
+    }
+   }
+
+   function resize2(visible) {
+    var elements = document.querySelectorAll('.online-photo');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle('online-photo-2', visible);
+    }
+   }
 
   document.querySelector('.hamburger').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleMenu()
+    toggleMenu();
+    resize();
+    resize2()
   });
